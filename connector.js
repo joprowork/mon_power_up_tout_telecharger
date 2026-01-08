644366fa3f3214484d0b3be00ada3602f0793e26
joprowork <joprowork@gmail.com>
2026-01-08 16:20:48 +0100

    V8 a tester

--- connector.js
+++ connector.js
@@ -1,12 +1,6 @@
-// Attachments Downloader - Power-Up Trello
-// Clé API Trello
 var TRELLO_API_KEY = '28d80811d05a80d515472323f02aab53';
 
-// URL de base du Power-Up (GitHub Pages)
-var BASE_URL = 'https://joprowork.github.io/mon_power_up_tout_telecharger';
-
 TrelloPowerUp.initialize({
-    // Section dans le dos de la carte
     'card-back-section': function(t, options) {
         return {
             title: '📥 Télécharger les pièces jointes',
